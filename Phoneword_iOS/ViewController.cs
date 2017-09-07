03377c0b4af3ebfa34c0b4e5f728bb57bbd89df4
Gerardo Flores <gerardo.flores@gmail.com>
2017-09-07 15:45:20 -0500

    Initial commit of sample application with unit tests

---
+++ Phoneword_iOS/ViewController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using UIKit;
+using Foundation;
+using System.Collections.Generic;
+
+namespace Phoneword_iOS
+{
+    public partial class ViewController : UIViewController
+    {
+		// translatedNumber was moved here from ViewDidLoad ()
+		string translatedNumber = "";
+
+		public List<string> PhoneNumbers { get; set; }
+
+		public ViewController(IntPtr handle) : base(handle)
+		{
+			//initialize list of phone numbers called for Call History screen
+			PhoneNumbers = new List<string>();
+
+		}
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			TranslateButton.TouchUpInside += (object sender, EventArgs e) => {
+				// Convert the phone number with text to a number
+				// using PhoneTranslator.cs
+				translatedNumber = PhoneTranslator.ToNumber(
+					PhoneNumberText.Text);
+
+				// Dismiss the keyboard if text field was tapped
+				PhoneNumberText.ResignFirstResponder();
+
+				if (translatedNumber == "")
+				{
+					CallButton.SetTitle("Call ", UIControlState.Normal);
+					CallButton.Enabled = false;
+				}
+				else
+				{
+					CallButton.SetTitle("Call " + translatedNumber,
+						UIControlState.Normal);
+					CallButton.Enabled = true;
+				}
+			};
+
+			CallButton.TouchUpInside += (object sender, EventArgs e) => {
+
+				//Store the phone number that we're dialing in PhoneNumbers
+				PhoneNumbers.Add(translatedNumber);
+
+
+				// Use URL handler with tel: prefix to invoke Apple's Phone app...
+				var url = new NSUrl("tel:" + translatedNumber);
+
+				// ...otherwise show an alert dialog
+				if (!UIApplication.SharedApplication.OpenUrl(url))
+				{
+					var alert = UIAlertController.Create("Not supported", "Scheme 'tel:' is not supported on this device", UIAlertControllerStyle.Alert);
+					alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
+					PresentViewController(alert, true, null);
+				}
+			};
+
+			CallHistoryButton.TouchUpInside += (object sender, EventArgs e) => {
+				// Launches a new instance of CallHistoryController
+				CallHistoryController callHistory = this.Storyboard.InstantiateViewController("CallHistoryController") as CallHistoryController;
+				if (callHistory != null)
+				{
+					callHistory.PhoneNumbers = PhoneNumbers;
+					this.NavigationController.PushViewController(callHistory, true);
+				}
+			};
+
+        }
+
+		//public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
+		//{
+		//	base.PrepareForSegue(segue, sender);
+
+		//	// set the View Controller that’s powering the screen we’re
+		//	// transitioning to
+
+		//	var callHistoryContoller = segue.DestinationViewController as CallHistoryController;
+
+		//	//set the Table View Controller’s list of phone numbers to the
+		//	// list of dialed phone numbers
+
+		//	if (callHistoryContoller != null)
+		//	{
+		//		callHistoryContoller.PhoneNumbers = PhoneNumbers;
+		//	}
+		//}
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ Phoneword_test/PhoneTranslatorTest.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using NUnit.Framework;
+using Phoneword_iOS;
+
+namespace Phoneword_test
+{
+    [TestFixture]
+    public class PhoneTranslatorTest
+    {
+        [Test]
+        public void ContainsReturnsTrueIfFound()
+        {
+            Assert.True(PhoneTranslator.Contains("something", 's'));
+        }
+
+        [Test]
+        public void ContainsReturnsFalseIfNotFound()
+        {
+            Assert.False(PhoneTranslator.Contains("something", 'z'));
+        }
+
+        [Test]
+        public void ContainsReturnsFalseIfBlank()
+        {
+            Assert.False(PhoneTranslator.Contains("something",' '));
+        }
+
+        [Test]
+        public void TranslateReturnsBlankIfNull()
+        {
+            Assert.AreSame("", PhoneTranslator.ToNumber(null));
+        }
+
+		[Test]
+		public void TranslateReturnsRightNumberForLowerCase()
+		{
+            Assert.AreEqual("2", PhoneTranslator.ToNumber("a"));
+		}
+
+		[Test]
+		public void TranslateIgnoresNonAlphanumeric()
+		{
+            Assert.AreSame("", PhoneTranslator.ToNumber(";"));
+		}
+
+    }
+}
